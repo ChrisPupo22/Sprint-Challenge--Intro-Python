89f96e9ff980e5b49904ae02b42ee98218c0e353
Christopher Pupo <ChrisPupo@Christophers-MacBook-Pro.local>
2020-02-14 11:32:20 -0500

    oop is done

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,33 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#Are we saying that Flight Vehicle and Starships are subclasses of Vehicle????? Very unclear...
+
+#Base class of Car and Motorcycle
+class Vehicle: 
+    def __init__(self):
+        pass
+
+class Car(Vehicle):
+    def __init__(self):
+                    pass
+
+class Motorcycle(Vehicle):
+    def __init__(self):
+                    pass
+
+#Base class of Airplane
+class FlightVehicle:
+    def __init__(self):
+        pass
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        pass
+
+#This is a Base class as well
+class Starship: 
+    def __init__(self):
+        pass
+
